5ccb3f3aff224f766356bf09b6816ba4fbd5a609
unknown <narmada.vaddireddy97@gmail.com>
2023-03-01 13:55:54 +0530

    Added: Code to print days reached to for month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,30 @@
 public class EmployeeWageComputation {
-    static final int FULL_TIME = 0;
-    static final int PART_TIME = 1;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
     public static void main(String[] args) {
-        int wagePerHour = 20;
-        int workingDay = 20;
-        int totalEmpWage = 0;
-        int attendance = (int) (Math.random() * 100) % 3;
-        int workingHour = 0;
-        for (int day = 1; day < workingDay; day++) {
-            switch (attendance) {
-                case FULL_TIME:
-                    workingHour = 8;
+        int empWage = 0, empHrs = 0, totalEmployeewage = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        int wagePerHour = 20, workingDay = 20;
+        while (totalEmpHrs < 100 && totalWorkingDays < workingDay) {
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
-                case PART_TIME:
-                    workingHour = 4;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
-                    workingHour=0;
+                    empHrs = 0;
             }
-            int empWage = workingHour * workingDay;
-            totalEmpWage = totalEmpWage + empWage;
+            totalEmpHrs = totalEmpHrs + empHrs;
+            empWage = empHrs * wagePerHour;
+            totalEmployeewage = totalEmployeewage + empWage;
         }
-        System.out.println("Employee Monthly wage: " + totalEmpWage + " Rupees.");
+        System.out.println("Total Employee WorkingDays is: " + totalWorkingDays);
+        System.out.println("Total Employee WorkingHour is: " + totalEmpHrs);
+        System.out.println("Total Employee Wage is: " + totalEmployeewage);
+
     }
 }
